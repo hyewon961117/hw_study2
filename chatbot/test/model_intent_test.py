c4f5fb9c29806c202cb692a5f3f6ca56796a2e77
hyewon <auddnjswls@naver.com>
2023-07-03 18:01:51 +0900

    2023.07.03.study

---
+++ chatbot/test/model_intent_test.py
@@ -0,0 +1,16 @@
+import sys
+sys.path.insert(0, 'C:\workspace\hw_study2\chatbot')
+
+from utils.Preprocess import Preprocess
+from models.intent.IntentModel import IntentModel
+
+p = Preprocess(word2index_dic='./train_tools/dict/chatbot_dict.bin', userdic='./utils/user_dic.tsv')
+
+intent = IntentModel(model_name='./models/intent/intent_model.h5', proprocess=p)
+query = '오늘 탕수육 주문 가능한가요?'
+predict = intent.predict_class(query)
+predict_label = intent.labels[predict]
+
+print(query)
+print('의도 예측 클래스:', predict)
+print('의도 예측 레이블:', predict_label)
